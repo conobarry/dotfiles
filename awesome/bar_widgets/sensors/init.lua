9d21bc46cc01e35b674bcc1977cc13b66c81d515
conobarry <52550366+conobarry@users.noreply.github.com>
2021-06-01 19:49:05 +0100

    Renaming of mb_temp to chipset_temp

--- awesome/bar_widgets/sensors/init.lua
+++ awesome/bar_widgets/sensors/init.lua
@@ -9,21 +9,25 @@ local cpu_usage = require("bar_widgets.sensors.cpu_usage")
 local gpu_temp = require("bar_widgets.sensors.gpu_temp")
 local gpu_usage = require("bar_widgets.sensors.gpu_usage")
 local memory_usage = require("bar_widgets.sensors.memory_usage")
-local mb_temp = require("bar_widgets.sensors.mb_temp")
+-- local mb_temp = require("bar_widgets.sensors.mb_temp")
+local chipset_temp = require("bar_widgets.sensors.chipset_temp")
 
 local cpu_temp_color = beautiful.rainbow_colors[1]
 local cpu_usage_color = beautiful.rainbow_colors[3]
 local memory_usage_color = beautiful.rainbow_colors[5]
 local gpu_temp_color = beautiful.rainbow_colors[6]
 local gpu_usage_color = beautiful.rainbow_colors[7]
-local mb_temp_color = beautiful.rainbow_colors[10]
+-- local mb_temp_color = beautiful.rainbow_colors[10]
+local chipset_temp_color = beautiful.rainbow_colors[10]
 
 local cpu_temp_widget = wibarutils.make_shape(wibarutils.left_parallelogram, cpu_temp, cpu_temp_color)
 local cpu_usage_widget = wibarutils.make_shape(wibarutils.left_parallelogram, cpu_usage, cpu_usage_color)
 local memory_usage_widget = wibarutils.make_shape(wibarutils.left_parallelogram, memory_usage, memory_usage_color)
 local gpu_temp_widget = wibarutils.make_shape(wibarutils.left_parallelogram, gpu_temp, gpu_temp_color)
 local gpu_usage_widget = wibarutils.make_shape(wibarutils.left_parallelogram, gpu_usage, gpu_usage_color)
-local mb_temp_widget = wibarutils.make_shape(wibarutils.left_parallelogram, mb_temp, mb_temp_color)
+-- local mb_temp_widget = wibarutils.make_shape(wibarutils.left_parallelogram, mb_temp, mb_temp_color)
+local chipset_temp_widget = wibarutils.make_shape(wibarutils.left_parallelogram, chipset_temp, chipset_temp_color)
+
 
 local sensors = wibox.widget {
   -- widget = wibox.container.margin,
@@ -37,7 +41,8 @@ local sensors = wibox.widget {
     memory_usage_widget,
     gpu_temp_widget,
     gpu_usage_widget,
-    mb_temp_widget,
+    -- mb_temp_widget,
+    chipset_temp_widget,
   -- }
 }
 
