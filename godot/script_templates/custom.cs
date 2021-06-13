46d47dc521b499eb831651bd3c41495ae7ce6871
conobarry <52550366+conobarry@users.noreply.github.com>
2021-06-13 19:53:31 +0100

    First commit

---
+++ godot/script_templates/custom.cs
@@ -0,0 +1,12 @@
+using Godot;
+
+public class %CLASS% : %BASE%
+{
+    
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+}
