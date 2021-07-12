7392db7d73b206950efe00b2eb3d938d657aea41
conobarry <52550366+conobarry@users.noreply.github.com>
2021-07-12 21:36:59 +0100

    Update for Neptune

--- awesome/bar_widgets/sensors/init.lua
+++ awesome/bar_widgets/sensors/init.lua
@@ -10,7 +10,7 @@ local gpu_temp = require("bar_widgets.sensors.gpu_temp")
 local gpu_usage = require("bar_widgets.sensors.gpu_usage")
 local memory_usage = require("bar_widgets.sensors.memory_usage")
 -- local mb_temp = require("bar_widgets.sensors.mb_temp")
-local chipset_temp = require("bar_widgets.sensors.chipset_temp")
+-- local chipset_temp = require("bar_widgets.sensors.chipset_temp")
 
 local cpu_temp_color = beautiful.rainbow_colors[1]
 local cpu_usage_color = beautiful.rainbow_colors[3]
@@ -18,7 +18,7 @@ local memory_usage_color = beautiful.rainbow_colors[5]
 local gpu_temp_color = beautiful.rainbow_colors[6]
 local gpu_usage_color = beautiful.rainbow_colors[7]
 -- local mb_temp_color = beautiful.rainbow_colors[10]
-local chipset_temp_color = beautiful.rainbow_colors[10]
+-- local chipset_temp_color = beautiful.rainbow_colors[10]
 
 local cpu_temp_widget = wibarutils.make_shape(wibarutils.left_parallelogram, cpu_temp, cpu_temp_color)
 local cpu_usage_widget = wibarutils.make_shape(wibarutils.left_parallelogram, cpu_usage, cpu_usage_color)
@@ -26,7 +26,7 @@ local memory_usage_widget = wibarutils.make_shape(wibarutils.left_parallelogram,
 local gpu_temp_widget = wibarutils.make_shape(wibarutils.left_parallelogram, gpu_temp, gpu_temp_color)
 local gpu_usage_widget = wibarutils.make_shape(wibarutils.left_parallelogram, gpu_usage, gpu_usage_color)
 -- local mb_temp_widget = wibarutils.make_shape(wibarutils.left_parallelogram, mb_temp, mb_temp_color)
-local chipset_temp_widget = wibarutils.make_shape(wibarutils.left_parallelogram, chipset_temp, chipset_temp_color)
+-- local chipset_temp_widget = wibarutils.make_shape(wibarutils.left_parallelogram, chipset_temp, chipset_temp_color)
 
 
 local sensors = wibox.widget {
@@ -42,7 +42,7 @@ local sensors = wibox.widget {
     gpu_temp_widget,
     gpu_usage_widget,
     -- mb_temp_widget,
-    chipset_temp_widget,
+    -- chipset_temp_widget,
   -- }
 }
 
